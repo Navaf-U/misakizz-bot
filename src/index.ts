1ce592cf3dd1ee04c67e5b82014f05be8d0c844a
navaf <navafsoft@gmail.com>
2025-04-19 10:47:12 +0530

    feat: implement core bot functionality with command loading and interaction handling

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {Client, GatewayIntentBits} from 'discord.js'
+import { token } from './config/botConfig'
+import { loadCommands } from './commands/commandLoader'
+import { handleInteraction } from './events/interactionCreate'
+import { Command } from './types/type';
+
+const client = new Client ({
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+let commands: Command[] = [];
+
+client.once('ready',async ()=>{
+    console.log(`${client.user?.tag} is online and ready!`)
+    commands = await loadCommands(); 
+    console.log(`Loaded ${commands.length} commands.`);
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    
+    await handleInteraction(interaction, commands);
+  });
+  
+  client.login(token);
