94300889ae1c6b34b98a08fbd695a6c873bbf5a5
navaf <navafsoft@gmail.com>
2025-04-19 12:56:54 +0530

    refactor: reorganize imports and improve code formatting in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,38 @@
-import {Client, GatewayIntentBits} from 'discord.js'
-import { token } from './config/botConfig'
-import { loadCommands } from './commands/commandLoader'
-import { handleInteraction } from './events/interactionCreate'
-import { Command } from './types/type';
-import dotenv from 'dotenv'
+import { Client, GatewayIntentBits } from "discord.js";
+import { loadCommands } from "./utils/commandLoader";
+import { handleInteraction } from "./events/interactionCreate";
+import { Command } from "./types/type";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const client = new Client ({
-    intents:[
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 let commands: Command[] = [];
 
-client.once('ready',async ()=>{
-    console.log(`${client.user?.tag} is online and ready!`)
-    commands = await loadCommands(); 
-    console.log(`Loaded ${commands.length} commands.`);
-})
+client.once("ready", async () => {
+  console.log(`${client.user?.tag} is online and ready!`);
+  commands = await loadCommands();
+  console.log(`Loaded ${commands.length} commands.`);
+});
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-    
-    await handleInteraction(interaction, commands);
-  });
-  
-  client.login(token);
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  await handleInteraction(interaction, commands);
+});
+
+const token = process.env.BOT_TOKEN;
+if (!token) {
+  throw new Error("Bot token is missing!");
+}
+
+client.login(token);
+
+
